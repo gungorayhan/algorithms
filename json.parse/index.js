e3978e69f31f81dae77fe52eaa8f2a70d4a5befa
AYHAN GÜNGÖR <gngr.ayhan@gmail.com>
2023-12-15 09:07:45 +0300

    json.parse

---
+++ json.parse/index.js
@@ -0,0 +1,16 @@
+let data ={
+    name:'ayhan',
+}
+
+console.log(data) //{ name: 'ayhan' }
+console.log(typeof data) //object
+
+let dataString= JSON.stringify(data);
+
+console.log(dataString) //{"name":"ayhan"} 
+console.log(typeof dataString) // string
+
+console.log(JSON.parse(dataString)) //{ name: 'ayhan' }
+
+//localStorage da bilgileri string olarak tutarız
+// window.localStorage.setItem("myObject", JSON.stringify(myObject));
